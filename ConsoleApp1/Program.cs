d178e98c4104026be0bd579c9b601bf1a79e101a
Mahsa-Sayadmanesh <118187586+Mahsa-Sayadmanesh@users.noreply.github.com>
2022-11-18 12:08:43 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello world!");
-            //comment
+            //comment 2
         }
     }
 }
